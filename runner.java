30222937eaf464c8cd02434d1f437c79841350dd
Tajniha8 <106667699+Tajniha8@users.noreply.github.com>
2022-12-05 20:06:07 +0530

    Add files via upload
    this is run

---
+++ runner.java
@@ -0,0 +1,17 @@
+package Testrun;
+
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (features="src/test/resources/features",glue= {"StepDefinition"},plugin = { "pretty",  
+		"html:target/cucumber-reports.html" },
+monochrome = true
+)
+public class runner {
+
+}
+
